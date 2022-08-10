704e9362ec622289e8cee961bff69eca127bf4e3
Your Name <angeltcastillo11@gmail.com>
2022-08-09 20:06:25 -0500

    added package.json

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(express.json());
 
-app.post('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-    let userNote = req.body;
-    userNote.id = Math.floor(Math.random() * 5000);
-    notes.push(userNote);
-  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-      res.json(userNote);
-  });
-  }); 
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 });
